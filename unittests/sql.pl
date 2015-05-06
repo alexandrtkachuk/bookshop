7af19eb7030abe1b04ce93cdfc719d445454375f
Your Name <you@example.com>
2015-05-06 05:37:07 +0300

    upadet

--- unittests/sql.pl
+++ unittests/sql.pl
@@ -1,52 +1,88 @@
 #!/usr/bin/perl 
 
+#test Models::Interfaces::Sql
 use warnings;
 use strict;
-
-use lib '../';
-use lib '../Libs';
-
-use Config::Config;
-use Models::Interfaces::Sql;
-#use Test::MockObject;
-
+use File::Basename;
+use constant TDIR=>dirname(__FILE__);
+use lib  '../Libs';
+use lib  '../';
+use base qw(Test::Class);
+use Test::MockObject;
 use Test::More 'no_plan';
+use Models::Interfaces::Sql;
 
-my $sql = Models::Interfaces::Sql->new();
-is($sql,0,'no create class, not params');
+my ($mockObj ,$sql, $dbh,$sth);
 
-$sql = Models::Interfaces::Sql->new('bdname','host','user','pass');
+sub startup : Test(startup)
+{
+    $mockObj = Test::MockObject->new();
+    $dbh = Test::MockObject->new();
+    $sth =Test::MockObject->new();
+    $sql =  Models::Interfaces::Sql->new();
+    is($sql,0,'no create class, not params');
+    $sql =  Models::Interfaces::Sql->new('bdname','host','user','pass');
+    ok($sql,'Create class SQL');
+    isa_ok($sql, 'Models::Interfaces::Sql');
+}
 
-ok($sql,'Create class SQL');
-isa_ok($sql, 'Models::Interfaces::Sql');
+sub sql_1_connect :Test
+{
+    can_ok($sql,'connect');
+    $mockObj->fake_module(
+        'DBI'=>( 'connect' => sub{0})  
+    );
+    is( $sql->connect(), 0, 'resul = 0 , param is wrong' );
+    $dbh->mock( 'disconnect', sub {1} );
+    $dbh->mock( 'quote', sub 
+        {
+            my ($self,$str)=@_;
+            return "'$str'";
+        } );
+
+    $mockObj->fake_module(
+        'DBI'=>( 'connect' => sub{return $dbh})  
+    );
+    is( $sql->connect(), 1, 'good connect' );
+}
+
+sub sql_2_select :Test
+{
+    can_ok($sql,'select');
+    is( $sql->select( ),0, 'return 0, no param' );
+    is($sql->select(['title','id','price'] ),1,' cearte select  ' );
+
+}
 
 
-can_ok($sql,'select');
-can_ok($sql,'connect');
-can_ok($sql,'getSql');
-can_ok($sql,'getRows');
-can_ok($sql,'getError');
-can_ok($sql, $_) for qw( OrederBy where);
+sub sql_3_setTeble :Test
+{
+    can_ok($sql,'setTable');
+    is($sql->setTable(),0, 'no param');
+    is($sql->setTable('shop_books'),1, 'set table');
+}
 
-is( $sql->connect(), 0, 'resul = 0 , param is wrong' );
+sub sql_4_where :Test
+{
+    can_ok($sql,'where');
+    is($sql->where('id',3),1,"set WHERE id = '3'");
+}
 
-isnt($sql->select(),1,'no connect to bd ' );
-is($sql->where(),0,'no connect to bd ' );
+sub sql_5_execute :Test
+{
+    can_ok($sql,'execute');
+    $sth->mock( 'execute', sub {1});
+    $dbh->mock( 'prepare', sub {return $sth;});
+    is($sql->execute(),1, 'query is create and is prepare  good');
+}
 
+sub sql_6_getSql :Test
+{
+    can_ok($sql,'getSql');
+    like($sql->getSql(), 
+        qr/^(SELECT *title, id, price *FROM shop_books *WHERE *id = '3') *$/, 
+        'true my query');
 
-$sql =  Models::Interfaces::Sql->new(
-        Config::Config::DBUSER,
-        Config::Config::DBHOST,
-        Config::Config::DBNAME,
-        Config::Config::DBPASS, 1);
+}
 
-is( $sql->connect(), 1, 'good connect for new copy Class' );
-is( $sql->select( ),0, 'return 0, no param' );
-is($sql->select(['title','id','price'] ),1,' cearte select  ' );
-is($sql->setTable(),0, 'no param');
-is($sql->setTable('shop_books'),1, 'set table');
-is($sql->where('id',3),1,"set WHERE id = '3'");
-is($sql->execute(),1, 'query is create and is prepare  good');
-like($sql->getSql(), 
-    qr/^(SELECT *title, id, price *FROM shop_books *WHERE *id = '3') *$/, 
-    'true my query');
+Test::Class->runtests;
